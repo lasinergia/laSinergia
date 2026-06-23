09b056148c14bfca8ef05686ae63337cf0c5b861
Matthew Becker <mateo62009mp@gmail.com>
2026-06-23 16:55:10 -0300

    Quick Fix
    Se reemplazo en Functions.js la linea 7

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,6 @@ function volverInteligente() {
   if (document.referrer && document.referrer !== window.location.href) {
     window.location.href = document.referrer;
   } else {
-    window.location.href = 'index.html'; 
+    window.location.href = 'https://lasinergia.com.co/'; 
   }
 }
